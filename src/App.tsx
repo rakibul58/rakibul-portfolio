24e77d9d04a96304634d84bef1ea687019e2c787
rakibul58 <rhrahi14@gmail.com>
2024-11-28 11:36:07 +0600

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { Navbar } from "./modules";
 import { Home, ProjectDetails } from "./pages";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-background text-foreground">
         <Navbar />
         <Routes>
